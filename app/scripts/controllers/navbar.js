b129e6866513ee1b0d5873af9179532ee7cca759
Jason Solis <jsolis@gmail.com>
2014-04-30 20:11:50 -0400

    yo angular-fullstack

---
+++ app/scripts/controllers/navbar.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('hubotioApp')
+  .controller('NavbarCtrl', function ($scope, $location) {
+    $scope.menu = [{
+      'title': 'Home',
+      'link': '/'
+    }];
+    
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+  });
