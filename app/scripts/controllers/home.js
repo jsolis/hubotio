b553cd7b038177d0d280afad346ad2fcb486a473
Jason Solis <jsolis@gmail.com>
2014-05-01 14:49:22 -0400

    parsed out URLs from each issue

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -3,6 +3,23 @@
 angular.module('hubotioApp')
   .controller('HomeCtrl', function ($scope, $http) {
     $http.get('https://api.github.com/repos/hubot-scripts/packages/issues').success(function(issues) {
+      console.log(issues);
+      for (var i=0; i < issues.length; i++) {
+        var issue = issues[i];
+        var body = issue.body;
+        var re = /(https?:\/\/[^\s]+)/g;
+        var matches;
+        var urls = {};
+        while ((matches = re.exec(body)) !== null) {
+          var url = matches[1];
+          if (url.indexOf('github') > -1) {
+            urls.github = url;
+          } else if (url.indexOf('npmjs') > -1) {
+            urls.npmjs = url;
+          }
+        }
+        issue.urls = urls;
+      }
       $scope.issues = issues;
     });
   });
